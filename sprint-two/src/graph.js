bc2efcc5ac8dbce1cef6847bf182dfbc511e415b
Alexander Vincent-Hill <alexanderjvincenthill@gmail.com>
2018-08-02 16:23:45 -0700

    Implement Graph with vertices and edges arrays

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,42 +1,46 @@
 // Instantiate a new graph
 var Graph = function() {
-  this.node = null;
+  this.vertices = [];
   this.edges = [];
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
-  this.node = node;
+  this.vertices.push(node);
+  this.edges[node] = [];
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
-  return this.node === node ? true : false;
+  return _.contains(this.vertices, node);
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
-  this.node = null;
-  this.edges = null;
+  this.vertices.pop(node);
+  this.edges[node] = []; // TBD
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
-  let areConnected = false;
-  if (fromNode && toNode) {
-    areConnected = this.fromNode.edges.contains(toNode);
-  }
-  return areConnected;
+  return _.contains(this.edges[fromNode], toNode);
 };
 
 // Connects two nodes in a graph by adding an edge between them.
-Graph.prototype.addEdge = function(fromNode, toNode) {};
+Graph.prototype.addEdge = function(fromNode, toNode) {
+  this.edges[fromNode].push(toNode);
+  this.edges[toNode].push(fromNode);
+};
 
 // Remove an edge between any two specified (by value) nodes.
-Graph.prototype.removeEdge = function(fromNode, toNode) {};
+Graph.prototype.removeEdge = function(fromNode, toNode) {
+  this.edges[toNode].pop(fromNode);
+};
 
 // Pass in a callback which will be executed on each node of the graph.
-Graph.prototype.forEachNode = function(cb) {};
+Graph.prototype.forEachNode = function(cb) {
+  _.each(this.nodes, cb); //TBD
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
