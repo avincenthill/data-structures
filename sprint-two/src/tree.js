b7e9790866892cf4275ed24437ed8855f0597586
Alexander Vincent-Hill <alexanderjvincenthill@gmail.com>
2018-08-02 00:39:56 -0700

    Pass tests through tree.contains

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,22 +1,38 @@
 var Tree = function(value) {
   var newTree = {};
+  if (!value) {
+    value = null;
+  }
   newTree.value = value;
 
-  // your code here
-  newTree.children = null;  // fix me
+  //share methods
+  newTree.addChild = treeMethods.addChild;
+  newTree.contains = treeMethods.contains;
 
+  newTree.children = {};
   return newTree;
 };
 
-var treeMethods = {};
+var treeMethods = {
+  addChild: function(value) {
+    this.children[Object.keys(this.children).length] = Tree(value);
+  },
 
-treeMethods.addChild = function(value) {
+  contains: function(target) {
+    //O(TBD) time complexity
+    //TBD: fix this
+    if (!this) {
+      return false;
+    } else if (this.value === target) {
+      return true;
+    } else if (this.children === {}) {
+      return false;
+    } else {
+      console.log(this.children[0]);
+      return this.children[0].contains(target);
+    }
+  }
 };
-
-treeMethods.contains = function(target) {
-};
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
